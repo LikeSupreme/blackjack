3dd5011b7a5e4310903ad89a4e1e6e1a60b1bdb3
Yusuf Eren Aydın <70768298+LargestWizard47@users.noreply.github.com>
2022-03-20 21:33:15 +0300

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+const kasa1 = document.querySelector("#kasa1");
+const kasa2 = document.querySelector("#kasa2");
+
+const oyuncu1 = document.querySelector("#oyuncu1");
+const oyuncu2 = document.querySelector("#oyuncu2");
