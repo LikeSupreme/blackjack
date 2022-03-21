cd18793b72800b680800214c27a90b44fde1592a
Yusuf Eren Aydın <70768298+LargestWizard47@users.noreply.github.com>
2022-03-21 23:38:44 +0300

    Update

--- script.js
+++ script.js
@@ -3,3 +3,43 @@ const kasa2 = document.querySelector("#kasa2");
 
 const oyuncu1 = document.querySelector("#oyuncu1");
 const oyuncu2 = document.querySelector("#oyuncu2");
+
+const start = document.querySelector("#start");
+const end = document.querySelector("#end");
+const add = document.querySelector("#add");
+
+
+
+const kartlar = [2, 3, 4, 5, 6, 7, 8, 9, "A", "K", "Q", "J"];
+
+start.addEventListener("click", play);
+
+function play(){
+
+    let randOyuncuKart1;
+    let randOyuncuKart2;
+
+    let randKasaKart1;
+    let randKasaKart2;
+
+    randOyuncuKart1 = Math.floor(Math.random() * kartlar.length);
+    randOyuncuKart2 = Math.floor(Math.random() * kartlar.length);
+
+    randKasaKart1 = Math.floor(Math.random() * kartlar.length);
+    randKasaKart2 = Math.floor(Math.random() * kartlar.length);
+
+    for (let i = 0; i < kartlar.length; i++) {
+        oyuncu1.textContent = kartlar[randOyuncuKart1];
+        oyuncu2.textContent = kartlar[randOyuncuKart2];
+
+        kasa1.textContent = kartlar[randKasaKart1];
+    }
+
+}
+
+add.addEventListener("click", addCard);
+
+function addCard(){
+
+}
+
